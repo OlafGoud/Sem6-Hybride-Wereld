07fe13c8870e90b4fccf3208c1636f4a125ef8ae
Jake-HBH <jakehersbach2005@gmail.com>
2026-04-20 09:54:12 +0200

    wijkzicht kiosk UI homescreen en mijlpalen paginas grotendeels gemaakt

---
+++ wijkzicht/wijkzicht/src/data/navigation.js
@@ -0,0 +1,17 @@
+export const quickLinks = [
+  {
+    title: 'TIJDLIJN BIJ BOUWPROJECT',
+    titleLines: ['TIJDLIJN BIJ', 'BOUWPROJECT'],
+    href: '/tijdlijn',
+  },
+  {
+    title: 'INFORMATIE BOUWPROJECT',
+    titleLines: ['INFORMATIE', 'BOUWPROJECT'],
+    href: '/info',
+  },
+]
+
+export const languageOptions = [
+  { label: 'NL', value: '/taal/nl' },
+  { label: 'EN', value: '/taal/en' },
+]
